ec28bda208f91f65c48aa6449045041647fd7d97
Chris Cooling <cmcooling@gmail.com>
2024-11-08 11:30:39 +0000

    Added Values

---
+++ 03_multiprocessing_scripts/value_example.py
@@ -0,0 +1,19 @@
+import multiprocessing
+import ctypes
+
+# Create a shared memory value
+# It is an integer with an initial value of 0
+v = multiprocessing.Value(ctypes.c_int, 0)
+
+def increment(v):
+    v.value += 1
+
+if __name__ == '__main__':
+    # Create a process that increments the value
+    p = multiprocessing.Process(target=increment, args=(v,))
+    p.start()
+    p.join()
+
+    # Print the value
+    print(v.value)
+
---
+++ 03_multiprocessing_scripts/value_lock_example.py
@@ -0,0 +1,32 @@
+import multiprocessing
+import ctypes
+
+# Create a shared memory value
+# It is an integer with an initial value of 0
+v = multiprocessing.Value(ctypes.c_int, 0)
+
+def increment(v):
+    # Manually acquire and release the lock
+    v.get_lock().acquire()
+    v.value += 1
+    v.get_lock().release()
+
+    # Use the context manager to acquire and release the lock
+    with v.get_lock():
+        v.value += 100
+
+if __name__ == '__main__':
+    # Create n_process processes which increment the value
+    n_process = 8
+    processes = []
+    for i in range(n_process):
+        p = multiprocessing.Process(target=increment, args=(v,))
+        p.start()
+        processes.append(p)
+
+    for i in range(n_process):
+        p.join()
+
+    # Print the value
+    print(v.value)
+
