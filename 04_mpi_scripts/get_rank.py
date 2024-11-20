8c00f01c942878a737161eafaf6841f8165e2bdb
Chris Cooling <cmcooling@gmail.com>
2024-11-20 09:57:17 +0000

    Introduction to MPI

---
+++ 04_mpi_scripts/get_rank.py
@@ -0,0 +1,10 @@
+import mpi4py.MPI as MPI
+
+# Get a reference to the current MPI.COMM_WORLD communicator
+comm = MPI.COMM_WORLD
+
+# Get the rank of the current process
+rank = comm.Get_rank()
+
+# Print the rank of the current process
+print(f'This script is being run by Rank {rank}')
