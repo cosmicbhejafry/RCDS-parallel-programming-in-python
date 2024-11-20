a7d030553ae38a9829a1f3ac3dad0ca22bd3188d
Chris Cooling <cmcooling@gmail.com>
2024-11-20 14:29:12 +0000

    Added getting total number of ranks

--- 04_mpi_scripts/get_rank.py
+++ 04_mpi_scripts/get_rank.py
@@ -5,8 +5,11 @@ import mpi4py.MPI as MPI
 # Get a reference to the current MPI.COMM_WORLD communicator
 comm = MPI.COMM_WORLD
 
+# Get the total number of ranks in the communicator
+n_rank = comm.Get_size()
+
 # Get the rank of the current process
 rank = comm.Get_rank()
 
 # Print the rank of the current process
-print(f'This script is being run by Rank {rank}')
+print(f'This script is being run by Rank {rank} out of {n_rank} total ranks')
