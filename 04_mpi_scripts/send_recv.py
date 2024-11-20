643b129625080cf5d125aab2b5405f6943c1ce32
Chris Cooling <cmcooling@gmail.com>
2024-11-20 14:31:34 +0000

    Added deadlock example

--- 04_mpi_scripts/send_recv.py
+++ 04_mpi_scripts/send_recv.py
@@ -2,12 +2,15 @@
 
 from mpi4py import MPI
 
+# Get the communicator and the rank of the process
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 if rank == 0:
+    # If we're in rank 0, send a dictionary to rank 1
     data = {'a': 7, 'b': 3.14}
     comm.send(data, dest=1)
 elif rank == 1:
+    # If we're in rank 1, receive the dictionary from rank 0
     data = comm.recv(source=0)
     print(rank, data)
