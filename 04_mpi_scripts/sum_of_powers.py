64a9b3f0aeb7b603bc1435936b499b489ccd8fa9
Chris Cooling <cmcooling@gmail.com>
2024-11-28 09:57:42 +0000

    Updated random array function

--- 04_mpi_scripts/sum_of_powers.py
+++ 04_mpi_scripts/sum_of_powers.py
@@ -1,5 +1,4 @@
 import numpy as np
 
-def random_integer_array(minimum, maximum, size):
-    return np.random.randint(minimum, maximum, size)
-
+def random_float_array(minimum, maximum, size):
+    return np.random.random(size) * (maximum - minimum) + minimum
