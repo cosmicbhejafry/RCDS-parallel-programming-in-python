f7b892ec8ef117c9f5d171f2807fd36f65e3df51
Chris Cooling <cmcooling@gmail.com>
2025-01-31 15:01:57 +0000

    Updated deadlock example

--- 04_mpi_scripts/deadlock.py
+++ 04_mpi_scripts/deadlock.py
@@ -7,15 +7,16 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 if rank == 0:
-    # Send a message to rank 1
-    comm.send("Hello from rank 0", dest=1)
     # Receive the message from rank 1
     data = comm.recv(source=1)
+    # Send a message to rank 1
+    comm.send("Hello from rank 0", dest=1)
 elif rank == 1:
-    # Send a message to rank 0
-    comm.send("Hello from rank 1", dest=0)
     # Receive the message from rank 0
     data = comm.recv(source=0)
+    # Send a message to rank 0
+    comm.send("Hello from rank 1", dest=0)
+    
 
 # This print statement will never be reached as the program will deadlock
 # You can force the code to stop Ctrl + C in the terminal
