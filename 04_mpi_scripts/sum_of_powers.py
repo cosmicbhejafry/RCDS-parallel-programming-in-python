601c2a5e94f310c70f101e5ae0d55ca73189a1b1
Chris Cooling <cmcooling@gmail.com>
2025-01-31 15:11:59 +0000

    Added dosctring to random_float_array

--- 04_mpi_scripts/sum_of_powers.py
+++ 04_mpi_scripts/sum_of_powers.py
@@ -1,4 +1,13 @@
 import numpy as np
 
 def random_float_array(minimum, maximum, size):
+    '''Generates a random array of floats between minimum and maximum.
+
+    Args:
+        minimum (float): minimum value which may be in the array
+        maximum (float): maximum value which may be in the array
+        size (int): size of the array
+        
+    Returns:
+        np.array: array of floats between minimum and maximum'''
     return np.random.random(size) * (maximum - minimum) + minimum
--- sample_solutions/sum_of_powers.py
+++ sample_solutions/sum_of_powers.py
@@ -6,6 +6,15 @@ from mpi4py import MPI
 import numpy as np
 
 def random_float_array(minimum, maximum, size):
+    '''Generates a random array of floats between minimum and maximum.
+
+    Args:
+        minimum (float): minimum value which may be in the array
+        maximum (float): maximum value which may be in the array
+        size (int): size of the array
+        
+    Returns:
+        np.array: array of floats between minimum and maximum'''
     return np.random.random(size) * (maximum - minimum) + minimum
 
 # Get the communicator and the rank of the process
